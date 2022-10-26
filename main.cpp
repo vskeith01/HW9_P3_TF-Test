861424cb4c5938798624f3304fe1080d5b0d069d
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-26 15:16:46 +0000

    plan

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/////////////////////// Funcitons ////////////////
+
+
+
+int main() {
+
+  
+  // creat array with the answeres
+  
+  // make function to coutn correct answers and output the number
+  // make function to put that into array 
+  // display array
+  
+  
+  }
