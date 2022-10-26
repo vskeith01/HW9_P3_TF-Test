5704b283554335169b0c1c03a6f054e8582f17f1
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-26 15:35:56 +0000

    aNSWER ARRAY

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -11,6 +13,12 @@ int main() {
 
   
   // creat array with the answeres
+  char student_answers[6][5] ={{'T','F','T','T','T'}
+                              {'T','T','T','T','T'}
+                              {'T','T','F','F','T'}
+                              {'F','T','F','F','F'}
+                              {'F','F','F','F','F'}
+                              {'T','T','F','T','F'}};
   
   // make function to coutn correct answers and output the number
   // make function to put that into array 
